224d134cf672e59fe8b741cd11385ad7109a65c4
Johnny-A09 <alvarado.johnny159@gmail.com>
2026-04-28 10:43:35 -0700

    back button most of the way there

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+// import {story} from "story.js";
+let position = [];
+
 function goTo(screen) {
     document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
     document.getElementById(screen).classList.add('active');
+    findPos();
 }
 
+function findPos(){
+    let current = document.querySelectorAll('active');
+    position.push(current);
+    console.log(position);
+};
